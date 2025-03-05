0f7a00226ecd97a7b30dfa6ec0e84c876c3543bf
PrathamManabasannanavar <pratham29052004@gmail.com>
2025-03-05 18:41:50 +0530

    fixed 2nd and 3rd row of XOX box

--- Projects/XOX_game/XOX.js
+++ Projects/XOX_game/XOX.js
@@ -2,27 +2,37 @@ let btns = document.getElementsByClassName("btns");
 let player1 =  document.getElementById("player1");
 let player2 =  document.getElementById("player2");
 
-let player = true;
-let arr = [0, 0, 0,  0, 0, 0,  0, 0, 0];
+var player = true;
+let arr = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
 
 function checkWinner(){
-        if (arr[0] === arr[1] && arr[1] === arr[2])
-			return arr[0];
-		if (arr[3] === arr[4] && arr[4] === arr[5])
-			return arr[3];
-		if (arr[6] === arr[7] && arr[7] === arr[8])
-			return arr[6];
-		if (arr[0] === arr[3] && arr[3] === arr[6])
-			return arr[0];
-		if (arr[1] === arr[4] && arr[4] === arr[7])
-			return arr[1];
-		if (arr[2] === arr[5] && arr[5] === arr[8])
-			return arr[2];
-		if (arr[0] === arr[4] && arr[4] === arr[8])
-			return arr[0];
-		if (arr[2] === arr[4] && arr[4] === arr[6])
-			return arr[2];
+        for(let i=0; i<3; i++){
+            if(arr[i][0] != 0 && arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]){
+                return arr[i][0];
+            }
+            if(arr[0][i] != 0 && arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]){
+                return arr[0][i];
+            }
+        }
+        if (arr[0][0] != 0 && arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2] && arr[0][0] !== 0) {
+            return arr[0][0];
+        }
+        if (arr[0][2] != 0 && arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0] && arr[0][2] !== 0) {
+            return arr[0][2];
+        }
         return 0;
+}
+
+function clearArray(){
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            arr[i][j] = 0;
+        }
+    }
 }
 
 function clearBox(){
@@ -30,8 +40,8 @@ function clearBox(){
     btnArray.forEach((btn) => {
     btn.disabled = false;
     btn.innerText = "";
-    arr = [0, 0, 0,  0, 0, 0,  0, 0, 0];
-    player = true;
+    clearArray();
+    window.player = true;
     })
 }
 function resetFunct(){
@@ -42,8 +52,8 @@ function resetFunct(){
     btnArray.forEach((btn) => {
     btn.disabled = false;
     btn.innerText = "";
-    arr = [0, 0, 0,  0, 0, 0,  0, 0, 0];
-    player = true;
+    clearArray()
+    window.player = true;
 });
 
 }
@@ -53,19 +63,21 @@ btnArray.forEach((btn) => {
     btn.addEventListener('click', (event)=>{
         const index = btnArray.indexOf(event.target);
         document.getElementById("resultBox").innerHTML = "";
+        // console.log(Math.floor(index/3), index%3);
+
         if(player){
             event.target.innerHTML = "<b>X</b>";
-            arr[index] = "X";
+            arr[Math.floor(index/3)][index%3] = "X";
             event.target.disabled = true;
-            player = false;
         }
         else{
             event.target.innerHTML = "<b>O</b>";
-            arr[index] = "O";
+            arr[Math.floor(index/3)][index%3] = "O";
             event.target.disabled = true;
-            player = true;
         }
-       
+        console.log(arr);
+        player = !player;
+        // console.log(player);
         let winner = checkWinner();
         if(winner == "O" || winner == "X"){
             if(winner == "X" ){
